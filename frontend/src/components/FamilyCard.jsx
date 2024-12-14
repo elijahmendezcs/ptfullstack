5c07d75b2d21433ca2b12702cbb3fd441fa12094
elijahmendezcs <elijahmendez2022@gmail.com>
2024-12-13 22:53:57 -0500

    cards added to page, need new fonts.

---
+++ frontend/src/components/FamilyCard.jsx
@@ -0,0 +1,101 @@
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Card from "@mui/material/Card";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import Button from "@mui/material/Button";
+
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme }) => ({
+  marginLeft: "auto",
+  transition: theme.transitions.create("transform", {
+    duration: theme.transitions.duration.shortest,
+  }),
+  variants: [
+    {
+      props: ({ expand }) => !expand,
+      style: {
+        transform: "rotate(0deg)",
+      },
+    },
+    {
+      props: ({ expand }) => !!expand,
+      style: {
+        transform: "rotate(180deg)",
+      },
+    },
+  ],
+}));
+
+export default function RecipeReviewCard() {
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  return (
+    <Card sx={{
+      maxWidth: 345,
+      borderRadius: 0, // Square corners
+      border: '0.5px solid lightgray' // Minimal light gray border
+    }}>
+      <CardMedia
+        component="img"
+        height="194"
+        image="/src/images/fam.jpg"
+        alt="default card"
+      />
+      <CardContent>
+        <Typography
+          variant="h6"
+          component="div"
+          sx={{
+            fontFamily: "'Cormorant Garamond', serif",
+            color: "text.primary",
+            marginBottom: 2,
+            fontWeight: 600,
+            fontStyle: "italic",
+          }}
+        >
+          Title of the Card
+        </Typography>
+        <Typography
+          variant="body2"
+          sx={{ color: "text.secondary", fontSize: 16 }}
+        >
+          2 hr 30 min
+        </Typography>
+        <Typography
+          variant="body2"
+          sx={{ color: "text.secondary", marginTop: 1, fontSize: 16 }}
+        >
+          $150
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <Button
+          variant="contained"
+          sx={{
+            backgroundColor: "black",
+            borderRadius: 0,
+            fontFamily: "'Cormorant Garamond', serif",
+            textTransform: "none",
+            "&:hover": {
+              backgroundColor: "black",
+            },
+            marginBottom: 2,
+            fontSize: 16,
+          }}
+        >
+          Book Now
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
--- frontend/src/pages/Book.jsx
+++ frontend/src/pages/Book.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
-import Card from "../components/Card";
+import React from "react";
+import BabyCard from "../components/BabyCard";
+import FamCard from "../components/FamilyCard";
+import PregCard from "../components/PregCard";
 
 const BookASession = () => {
   return (
-    <div>
-      <Card/>
-      <Card/>
-      <Card/>
+    <div className="flex justify-center items-center gap-4 mt-24">
+      <FamCard />
+      <PregCard />
+      <BabyCard />
     </div>
-  )
-}
+  );
+};
 
-export default BookASession
+export default BookASession;
