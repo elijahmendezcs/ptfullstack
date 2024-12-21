4aa7c5e39fd85a167146a6a98f6ae26c08c4a2bb
Justin Olivacce <justin.olivacce@mykenzie.snhu.edu>
2024-12-21 00:05:01 -0600

    changed photos on Senior page

--- frontend/src/components/SeniorImageList.jsx
+++ frontend/src/components/SeniorImageList.jsx
@@ -2,6 +2,19 @@ import * as React from 'react';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
 
+import seniorImg from '../images/SeniorImages/senior1.jpg'
+import seniorImg2 from '../images/SeniorImages/senior2.jpg'
+import seniorImg3 from '../images/SeniorImages/senior3.jpg'
+import seniorImg4 from '../images/SeniorImages/senior4.jpg'
+import seniorImg5 from '../images/SeniorImages/senior5.jpg'
+import seniorImg6 from '../images/SeniorImages/senior6.jpg'
+import seniorImg7 from '../images/SeniorImages/senior7.jpg'
+import seniorImg8 from '../images/SeniorImages/senior8.jpg'
+import seniorImg9 from '../images/SeniorImages/senior9.jpg'
+import seniorImg10 from '../images/SeniorImages/senior10.jpg'
+import seniorImg11 from '../images/SeniorImages/senior11.jpg'
+import seniorImg12 from '../images/SeniorImages/senior12.jpg'
+
 function srcset(image, size, rows = 1, cols = 1) {
   return {
     src: `${image}?w=${size * cols}&h=${size * rows}&fit=crop&auto=format`,
@@ -44,60 +57,60 @@ export default function QuiltedImageList() {
 
 const itemData = [
   {
-    img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
-    title: 'Breakfast',
+    img: seniorImg,
+    title: 'Senior Male Sitting',
     rows: 2,
     cols: 2,
   },
   {
-    img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
-    title: 'Burger',
+    img: seniorImg2,
+    title: 'Senior Female Sitting',
   },
   {
-    img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
-    title: 'Camera',
+    img: seniorImg3,
+    title: 'Male Senior Trees',
   },
   {
-    img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
-    title: 'Coffee',
+    img: seniorImg4,
+    title: 'MaleSeniorCapGown',
     cols: 2,
   },
   {
-    img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
-    title: 'Hats',
+    img: seniorImg5,
+    title: 'SeniorBench',
     cols: 2,
   },
   {
-    img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
-    title: 'Honey',
+    img: seniorImg6,
+    title: 'FemaleSeniorCapGown',
     rows: 2,
     cols: 2,
   },
   {
-    img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
-    title: 'Basketball',
+    img: seniorImg7,
+    title: 'SeniorFeild',
   },
   {
-    img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
-    title: 'Fern',
+    img: seniorImg8,
+    title: 'SeniorPark',
   },
   {
-    img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
-    title: 'Mushrooms',
+    img: seniorImg9,
+    title: 'SeniorLaying',
     rows: 2,
     cols: 2,
   },
   {
-    img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
-    title: 'Tomato basil',
+    img: seniorImg10,
+    title: 'SeniorBuilding',
   },
   {
-    img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
-    title: 'Sea star',
+    img: seniorImg11,
+    title: 'SeniorBuilding2',
   },
   {
-    img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
-    title: 'Bike',
+    img: seniorImg12,
+    title: 'FemaleSeniorSunny',
     cols: 2,
   },
 ];
