74f8c95a7a8a2736497d37a5e5939181f482bd5a
Justin <justin.olivacce@mykenzie.snhu.edu>
2024-12-25 17:04:42 -0600

    added title to senior, but it is not in the correct place needs work

--- frontend/src/pages/Senior.jsx
+++ frontend/src/pages/Senior.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 import ImageList from "../components/SeniorImageList"
+import { Typography } from '@mui/material'
 const Senior = () => {
   return (
        <div className="flex items-center justify-center h-screen">
+        <Typography
+        sx={{
+          fontFamily: '"Cormorant Garamond", serif',
+          fontSize: "30px",
+          fontStyle: "italic",
+          fontWeight: 400,
+          marginTop: "20px",
+        }}
+         >
+          Senior 
+        </Typography >
          <ImageList />
        </div>
      )
