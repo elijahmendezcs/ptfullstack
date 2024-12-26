da1b1d0fd5f41881a2da62f716ed6684b5b8b004
Justin <justin.olivacce@mykenzie.snhu.edu>
2024-12-25 18:21:37 -0600

    fixed the senior title, but code may need some work

--- frontend/src/pages/Senior.jsx
+++ frontend/src/pages/Senior.jsx
@@ -3,8 +3,9 @@ import ImageList from "../components/SeniorImageList"
 import { Typography } from '@mui/material'
 const Senior = () => {
   return (
-       <div className="flex items-center justify-center h-screen">
-        <Typography
+    <><div className="flex flex-col items-left justify-center flex-grow px-20">
+
+      <Typography
         sx={{
           fontFamily: '"Cormorant Garamond", serif',
           fontSize: "30px",
@@ -12,11 +13,13 @@ const Senior = () => {
           fontWeight: 400,
           marginTop: "20px",
         }}
-         >
-          Senior 
-        </Typography >
-         <ImageList />
-       </div>
+      >
+        Senior
+      </Typography>
+    </div>
+    <div className='flex items-center justify-center h-screen'>
+        <ImageList />
+      </div></>
      )
   
 }
