aa0ff7333ac9ce943793aa373effb787f33e3522
elijahmendezcs <elijahmendez2022@gmail.com>
2025-01-03 23:50:20 -0500

    commiting for now because I might break the code.

--- frontend/src/pages/Prints.jsx
+++ frontend/src/pages/Prints.jsx
@@ -1,56 +1,54 @@
 import React from "react";
-import BabyCard from "../components/BabyCard";
-import FamCard from "../components/FamilyCard";
-import PregCard from "../components/PregCard";
+import Card from "../components/Card";
 
 const Prints = () => {
   return (
     <div className="flex flex-col items-center gap-10 mt-24 mb-24">
       {/* First Row */}
       <div className="flex justify-center items-center gap-10 row-style-1">
-        <FamCard />
-        <PregCard />
-        <BabyCard />
+        <Card />
+        <Card />
+        <Card />
       </div>
 
       {/* Second Row */}
       <div className="flex justify-center items-center gap-10 row-style-2">
-        <FamCard />
-        <PregCard />
-        <BabyCard />
+        <Card />
+        <Card />
+        <Card />
       </div>
 
       {/* Third Row */}
       <div className="flex justify-center items-center gap-10 row-style-3">
-        <FamCard />
-        <PregCard />
-        <BabyCard />
+        <Card />
+        <Card />
+        <Card />
       </div>
 
       {/* Fourth Row */}
       <div className="flex justify-center items-center gap-10 row-style-4">
-        <FamCard />
-        <PregCard />
-        <BabyCard />
+        <Card />
+        <Card />
+        <Card />
       </div>
 
       {/* Fifth Row */}
       <div className="flex justify-center items-center gap-10 row-style-5">
-        <FamCard />
-        <PregCard />
-        <BabyCard />
+        <Card />
+        <Card />
+        <Card />
       </div>
 
       {/* Sixth Row */}
       <div className="flex justify-center items-center gap-10 row-style-5">
-        <FamCard />
-        <PregCard />
-        <BabyCard />
+        <Card />
+        <Card />
+        <Card />
       </div>
 
       {/* Seventh Row */}
       <div className="flex justify-center items-center gap-10 row-style-5">
-        <FamCard />
+        <Card />
       </div>
     </div>
   );
