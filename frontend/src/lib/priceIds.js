6bc1bd61425397c3c70fdff707a14f216b5efb65
Elijah Mendez <elijahmendez2022@gmail.com>
2025-01-23 21:56:37 -0500

    added all new price ids.

--- frontend/src/lib/priceIds.js
+++ frontend/src/lib/priceIds.js
@@ -1,12 +1,34 @@
 export const priceIds = {
-  BlackAndWhite1: "price_1QkZde06SIMuWkYgYBzqc8Iz",
-  BlackAndWhite2: "price_1QkZd706SIMuWkYgDLV6qgNF",
-  BlackAndWhite3: "price_1QkZcg06SIMuWkYgxuzoZlRs",
-  BlackAndWhite4: "price_1QkZb306SIMuWkYgK8LZdLeJ",
-  BlackAndWhite5: "price_1QkZag06SIMuWkYg0roL0sHs",
-  BlackAndWhite6: "price_1QkZaM06SIMuWkYgz2PcIGau",
-  BlackAndWhite7: "price_1QkZZ706SIMuWkYgTFxkNq3l",
-  BlackAndWhite8: "price_1QkZRn06SIMuWkYgAHNoSchO",
+
+  // 8x10 Prints
+  BW1_8x10: "price_1Qkcap06SIMuWkYg6YSTiCSY",
+  BW2_8x10: "price_1Qkcb106SIMuWkYgzpkTo7Px",
+  BW3_8x10: "price_1QkcbW06SIMuWkYgRBwLYvJi",
+  BW4_8x10: "price_1Qkcbh06SIMuWkYg1JLjd2l6",
+  BW5_8x10: "price_1Qkcbr06SIMuWkYgjTHSvPa3",
+  BW6_8x10: "price_1Qkcc306SIMuWkYgByPP1orJ",
+  BW7_8x10: "price_1QkccF06SIMuWkYgdLmIAa8w",
+  BW8_8x10: "price_1QkccQ06SIMuWkYgidnCgHdS",
+
+  // 11x14 Prints
+  BW1_11x14: "price_1Qkcco06SIMuWkYgH1KDMOCT",
+  BW2_11x14: "price_1Qkccy06SIMuWkYgmofgX5Hc",
+  BW3_11x14: "price_1QkcdB06SIMuWkYgcMfDxUcX",
+  BW4_11x14: "price_1Qkcdg06SIMuWkYg7YoHGtSL",
+  BW5_11x14: "price_1Qkcds06SIMuWkYgscNgH8k4",
+  BW6_11x14: "price_1Qkce306SIMuWkYgmAJogA2m",
+  BW7_11x14: "price_1QkcfV06SIMuWkYgzDgxFd2F",
+  BW8_11x14: "price_1Qkcff06SIMuWkYgnEQ9FG6O",
+
+  // 16x20 Prints
+  BW1_16x20: "price_1QkcgO06SIMuWkYgYFsvc2zF",
+  BW2_16x20: "price_1Qkcga06SIMuWkYg8I36Wqeh",
+  BW3_16x20: "price_1Qkcgm06SIMuWkYg6FFB4Gkf",
+  BW4_16x20: "price_1QkchU06SIMuWkYga7Lb6nP8",
+  BW5_16x20: "price_1Qkchf06SIMuWkYgFvlYeY0F",
+  BW6_16x20: "price_1Qkchp06SIMuWkYgMqQPwGO6",
+  BW7_16x20: "price_1Qkci906SIMuWkYgojfkdrb6",
+  BW8_16x20: "price_1Qkcid06SIMuWkYgx9KoYZMh",
 };
 
 // UPDATED PRICE IDS TO THE $25 ONES
