379a13699bae08c5f12955dd2cee532932dfccd6
Elijah Mendez <elijahmendez2022@gmail.com>
2025-01-24 11:41:30 -0500

    Added shipping options to stripe checkout.

--- backend/routes/stripe.js
+++ backend/routes/stripe.js
@@ -27,6 +27,36 @@ router.post("/create-checkout-session", async (req, res) => {
           quantity,
         },
       ],
+      // Stripe will display the shipping address form on the Checkout page:
+      shipping_address_collection: {
+        // Set which countries you ship to:
+        allowed_countries: ["US", "CA"],
+      },
+      // Define your shipping rate(s) inline:
+      shipping_options: [
+        {
+          shipping_rate_data: {
+            display_name: "Flat Shipping",
+            type: "fixed_amount",
+            fixed_amount: {
+              amount: 500, // $5.00 in cents
+              currency: "usd",
+            },
+            // Optionally set a delivery estimate (shown on the checkout page)
+            delivery_estimate: {
+              minimum: {
+                unit: "business_day",
+                value: 3,
+              },
+              maximum: {
+                unit: "business_day",
+                value: 5,
+              },
+            },
+          },
+        },
+      ],
+      // Where to redirect upon success/cancel:
       success_url: "http://localhost:5173/thankyou",
       cancel_url: "http://localhost:5173/cancel",
     });
