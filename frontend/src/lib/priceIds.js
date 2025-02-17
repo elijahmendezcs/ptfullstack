d220fd4654de498d16f1d0d843c18f7d470fbed5
Elijah Mendez <elijahmendez2022@gmail.com>
2025-02-17 13:17:44 -0500

    updates to price ids.

--- frontend/src/lib/priceIds.js
+++ frontend/src/lib/priceIds.js
@@ -1,73 +1,76 @@
 export const priceIds = {
   // 8x10 Prints
-  BW1_8x10: "price_1Qkcap06SIMuWkYg6YSTiCSY",
-  BW2_8x10: "price_1Qkcb106SIMuWkYgzpkTo7Px",
-  BW3_8x10: "price_1QkcbW06SIMuWkYgRBwLYvJi",
-  BW4_8x10: "price_1Qkcbh06SIMuWkYg1JLjd2l6",
-  BW5_8x10: "price_1Qkcbr06SIMuWkYgjTHSvPa3",
-  BW6_8x10: "price_1Qkcc306SIMuWkYgByPP1orJ",
-  BW7_8x10: "price_1QkccF06SIMuWkYgdLmIAa8w",
-  BW8_8x10: "price_1QkccQ06SIMuWkYgidnCgHdS",
-  BW11_8x10: "price_1QpL8M06SIMuWkYgtzOS9zeD",
-  BW12_8x10: "price_1QpWT806SIMuWkYgIwD9mYvb",
-  BW13_8x10: "price_1QpWTM06SIMuWkYgnG0H3eeG",
-  BW14_8x10: "price_1QpWTe06SIMuWkYgG9BgTLYj",
+  BW1_8x10: "price_1Qkcap06SIMuWkYg6YSTiCSY", // Croghan
+  BW2_8x10: "price_1Qkcb106SIMuWkYgzpkTo7Px", // Serene
+  BW3_8x10: "price_1QkcbW06SIMuWkYgRBwLYvJi", // Tindall Bridge Markers
+  BW4_8x10: "price_1Qkcbh06SIMuWkYg1JLjd2l6", // Deserted
+  BW5_8x10: "price_1Qkcbr06SIMuWkYgjTHSvPa3", // I Give You Grace
+  BW6_8x10: "price_1Qkcc306SIMuWkYgByPP1orJ", // Tindall Bridge
+  BW7_8x10: "price_1QkccF06SIMuWkYgdLmIAa8w", // Simple Life
+  BW8_8x10: "price_1QkccQ06SIMuWkYgidnCgHdS", // Aftermath
+  BW11_8x10: "price_1QpL8M06SIMuWkYgtzOS9zeD", // Kitch-iti-kipi
+  BW12_8x10: "price_1QpWT806SIMuWkYgIwD9mYvb", // Never Alone
+  BW13_8x10: "price_1QpWTM06SIMuWkYgnG0H3eeG", // Sound Of Silence
+  BW14_8x10: "price_1QpWTe06SIMuWkYgG9BgTLYj", // Sandusky Bay Cattails
 
   // 11x14 Prints
-  BW1_11x14: "price_1Qkcco06SIMuWkYgH1KDMOCT",
-  BW2_11x14: "price_1Qkccy06SIMuWkYgmofgX5Hc",
-  BW3_11x14: "price_1QkcdB06SIMuWkYgcMfDxUcX",
-  BW4_11x14: "price_1Qkcdg06SIMuWkYg7YoHGtSL",
-  BW5_11x14: "price_1Qkcds06SIMuWkYgscNgH8k4",
-  BW6_11x14: "price_1Qkce306SIMuWkYgmAJogA2m",
-  BW7_11x14: "price_1QkcfV06SIMuWkYgzDgxFd2F",
-  BW8_11x14: "price_1Qkcff06SIMuWkYgnEQ9FG6O",
-  BW11_11x14: "price_1QpWU406SIMuWkYgHxhCWjCt",
-  BW12_11x14: "price_1QpWUJ06SIMuWkYgfTyxmwYF",
-  BW13_11x14: "price_1QpWUi06SIMuWkYggDaXAE6g",
-  BW14_11x14: "price_1QpWV706SIMuWkYgJXOLHDAy",
+  BW1_11x14: "price_1Qkcco06SIMuWkYgH1KDMOCT", // Croghan
+  BW2_11x14: "price_1Qkccy06SIMuWkYgmofgX5Hc", // Serene
+  BW3_11x14: "price_1QkcdB06SIMuWkYgcMfDxUcX", // Tindal Bridge Markers
+  BW4_11x14: "price_1Qkcdg06SIMuWkYg7YoHGtSL", // Deserted
+  BW5_11x14: "price_1Qkcds06SIMuWkYgscNgH8k4", // I Give You Grace
+  BW6_11x14: "price_1Qkce306SIMuWkYgmAJogA2m", // Tindall Bridge
+  BW7_11x14: "price_1QkcfV06SIMuWkYgzDgxFd2F", // Simple Life
+  BW8_11x14: "price_1Qkcff06SIMuWkYgnEQ9FG6O", // Aftermath
+  BW11_11x14: "price_1QpWU406SIMuWkYgHxhCWjCt", // Kitch-iti-kipi
+  BW12_11x14: "price_1QpWUJ06SIMuWkYgfTyxmwYF", // Never Alone
+  BW13_11x14: "price_1QpWUi06SIMuWkYggDaXAE6g", // Sound Of Silence
+  BW14_11x14: "price_1QpWV706SIMuWkYgJXOLHDAy", // Sandusky Bay Cattails
 
   // 16x20 Prints
-  BW1_16x20: "price_1QkcgO06SIMuWkYgYFsvc2zF",
-  BW2_16x20: "price_1Qkcga06SIMuWkYg8I36Wqeh",
-  BW3_16x20: "price_1Qkcgm06SIMuWkYg6FFB4Gkf",
-  BW4_16x20: "price_1QkchU06SIMuWkYga7Lb6nP8",
-  BW5_16x20: "price_1Qkchf06SIMuWkYgFvlYeY0F",
-  BW6_16x20: "price_1Qkchp06SIMuWkYgMqQPwGO6",
-  BW7_16x20: "price_1Qkci906SIMuWkYgojfkdrb6",
-  BW8_16x20: "price_1Qkcid06SIMuWkYgx9KoYZMh",
-  BW11_16x20: "price_1QpWW206SIMuWkYgL4RzOPpg",
-  BW12_16x20: "price_1QpWWI06SIMuWkYgOLfer6TW",
-  BW13_16x20: "price_1QpWWa06SIMuWkYgEiPRxhn9",
-  BW14_16x20: "price_1QpWWr06SIMuWkYgQpHLSmDH",
+  BW1_16x20: "price_1QkcgO06SIMuWkYgYFsvc2zF", // Croghan
+  BW2_16x20: "price_1Qkcga06SIMuWkYg8I36Wqeh", // Serene
+  BW3_16x20: "price_1Qkcgm06SIMuWkYg6FFB4Gkf", // Tindall Bridge Markers
+  BW4_16x20: "price_1QkchU06SIMuWkYga7Lb6nP8", // Deserted
+  BW5_16x20: "price_1Qkchf06SIMuWkYgFvlYeY0F", // I Give You Grace
+  BW6_16x20: "price_1Qkchp06SIMuWkYgMqQPwGO6", // Tindall Bridge
+  BW7_16x20: "price_1Qkci906SIMuWkYgojfkdrb6", // Simple Life
+  BW8_16x20: "price_1Qkcid06SIMuWkYgx9KoYZMh", // Aftermath
+  BW11_16x20: "price_1QpWW206SIMuWkYgL4RzOPpg", // Kitch-iti-kipi
+  BW12_16x20: "price_1QpWWI06SIMuWkYgOLfer6TW", // Never Alone
+  BW13_16x20: "price_1QpWWa06SIMuWkYgEiPRxhn9", // Sound Of Silence
+  BW14_16x20: "price_1QpWWr06SIMuWkYgQpHLSmDH", // Sandusky Bay Cattails
 
-  A1_8x10: "price_1QlymQ06SIMuWkYgfS07yc6S",
-  A2_8x10: "price_1Qlymi06SIMuWkYgjWB56L2E",
-  A3_8x10: "price_1Qlymw06SIMuWkYgvmeV60yd",
-  A4_8x10: "price_1QlynA06SIMuWkYgSBRQDi3Q",
-  A5_8x10: "price_1QlynO06SIMuWkYgcSBtKiho",
-  A6_8x10: "price_1Qlync06SIMuWkYgDSG29Atg",
-  A7_8x10: "price_1Qlynu06SIMuWkYgr1rhfDkr",
-  A8_8x10: "price_1QpKwm06SIMuWkYgHAyZW8KR",
-  A9_8x10: "price_1QpKx206SIMuWkYgPQQAqE5x",
+  // 8x10 Prints
+  A1_8x10: "price_1QlymQ06SIMuWkYgfS07yc6S", // St. Joseph's Catholic School
+  A2_8x10: "price_1Qlymi06SIMuWkYgjWB56L2E", // Nazareth Hall
+  A3_8x10: "price_1Qlymw06SIMuWkYgvmeV60yd", // Nazareth Hall Chapel
+  A4_8x10: "price_1QlynA06SIMuWkYgSBRQDi3Q", // Nazareth Hall Stained Glass
+  A5_8x10: "price_1QlynO06SIMuWkYgcSBtKiho", // Muse
+  A6_8x10: "price_1Qlync06SIMuWkYgDSG29Atg", // The Spirit Lives In You
+  A7_8x10: "price_1Qlynu06SIMuWkYgr1rhfDkr", // Toledo Repertoire Theatre
+  A8_8x10: "price_1QpKwm06SIMuWkYgHAyZW8KR", // Anthony Wayne Bridge
+  A9_8x10: "price_1QpKx206SIMuWkYgPQQAqE5x", // Rosary Cathedral
 
-  A1_11x14: "price_1Qlyps06SIMuWkYgGZdTzWUc",
-  A2_11x14: "price_1Qlyq506SIMuWkYg9YtmnbDQ",
-  A3_11x14: "price_1QlyqJ06SIMuWkYgk3PRuscH",
-  A4_11x14: "price_1QlyqV06SIMuWkYgnESEUduB",
-  A5_11x14: "price_1Qlyqf06SIMuWkYg2PddghW2",
-  A6_11x14: "price_1Qlyqt06SIMuWkYgIlVJfI4Y",
-  A7_11x14: "price_1Qlyr506SIMuWkYg9Ao1ywpn",
-  A8_11x14: "price_1QpKxI06SIMuWkYgnM0m6Ozs",
-  A9_11x14: "price_1QpKxU06SIMuWkYgQLkQznfO",
-
-  A1_16x20: "price_1QlyrR06SIMuWkYg19aGIwoE",
-  A2_16x20: "price_1Qlyrf06SIMuWkYg8mlWeGtS",
-  A3_16x20: "price_1Qlyrw06SIMuWkYgfrm8tpka",
-  A4_16x20: "price_1Qlyv206SIMuWkYggw3AH8eE",
-  A5_16x20: "price_1QlyvJ06SIMuWkYgav4BBImp",
-  A6_16x20: "price_1QlyvV06SIMuWkYgCz245FxI",
-  A7_16x20: "price_1Qlyvk06SIMuWkYgTQ2xdg02",
-  A8_16x20: "price_1QpL0Z06SIMuWkYgkdXkpyvl",
-  A9_16x20: "price_1QpL0o06SIMuWkYgXHCMsorw",
+  // 11x14 Prints
+  A1_11x14: "price_1Qlyps06SIMuWkYgGZdTzWUc", // St. Joseph's Catholic School
+  A2_11x14: "price_1Qlyq506SIMuWkYg9YtmnbDQ", // Nazareth Hall
+  A3_11x14: "price_1QlyqJ06SIMuWkYgk3PRuscH", // Nazareth Hall Chapel
+  A4_11x14: "price_1QlyqV06SIMuWkYgnESEUduB", // Nazareth Hall Stained Glass
+  A5_11x14: "price_1Qlyqf06SIMuWkYg2PddghW2", // Muse
+  A6_11x14: "price_1Qlyqt06SIMuWkYgIlVJfI4Y", // The Spirit Lives In You
+  A7_11x14: "price_1Qlyr506SIMuWkYg9Ao1ywpn", // Toledo Repertoire Theatre
+  A8_11x14: "price_1QpKxI06SIMuWkYgnM0m6Ozs", // Anthony Wayne Bridge
+  A9_11x14: "price_1QpKxU06SIMuWkYgQLkQznfO", // Rosary Cathedral
+  
+  // 16x20 Prints
+  A1_16x20: "price_1QlyrR06SIMuWkYg19aGIwoE", // St. Joseph's Catholic School
+  A2_16x20: "price_1Qlyrf06SIMuWkYg8mlWeGtS", // Nazareth Hall
+  A3_16x20: "price_1Qlyrw06SIMuWkYgfrm8tpka", // Nazareth Hall Chapel
+  A4_16x20: "price_1Qlyv206SIMuWkYggw3AH8eE", // Nazareth Hall Stained Glass
+  A5_16x20: "price_1QlyvJ06SIMuWkYgav4BBImp", // Muse
+  A6_16x20: "price_1QlyvV06SIMuWkYgCz245FxI", // The Spirit Lives In You
+  A7_16x20: "price_1Qlyvk06SIMuWkYgTQ2xdg02", // Toledo Repertoire Theatre
+  A8_16x20: "price_1QpL0Z06SIMuWkYgkdXkpyvl", // Anthony Wayne Bridge
+  A9_16x20: "price_1QpL0o06SIMuWkYgXHCMsorw", // Rosary Cathedral
 };
