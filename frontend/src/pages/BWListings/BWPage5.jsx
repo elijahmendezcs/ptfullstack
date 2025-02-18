e7b4933083f1e4eea84691a7a38d5563879d48c8
Elijah Mendez <elijahmendez2022@gmail.com>
2025-02-17 22:34:37 -0500

    another capitilization change.

--- frontend/src/pages/BWListings/BWPage5.jsx
+++ frontend/src/pages/BWListings/BWPage5.jsx
@@ -12,7 +12,7 @@ import {
 } from "@/components/ui/card";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
-import blackandwhite from "../../images/BlackAndWhiteImages/blackandwhite7.jpg";
+import blackandwhite from "../../images/BlackAndWhiteImages/blackandwhite7.JPG";
 
 const BWPage1 = () => {
   const [selectedSize, setSelectedSize] = useState(null);
